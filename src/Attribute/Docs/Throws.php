d1f97d183c2b99f5a0a13bd98dad34bbee47439e
BaD ClusteR <spammer_90@mail.ru>
2025-06-28 21:26:35 +0400

    Attribute\Docs\Throws is now repeatable.

--- src/Attribute/Docs/Throws.php
+++ src/Attribute/Docs/Throws.php
@@ -4,7 +4,7 @@ namespace ApiPlatform\Attribute\Docs;
 
 use Attribute;
 
-#[Attribute(Attribute::TARGET_METHOD)]
+#[Attribute(Attribute::TARGET_METHOD | Attribute::IS_REPEATABLE)]
 readonly class Throws
 {
     public function __construct(
